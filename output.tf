8eca8522b612fd9365d4e29120826870f9afad45
Hristo Ganekov <hristo.ganekov@sumup.com>
2020-07-06 15:40:46 +0300

    changing principal assoc to be a for_reach so order of principals dont cause recreation of objects

--- output.tf
+++ output.tf
@@ -88,5 +88,6 @@ output "this_ram_resource_share_id" {
 // aws_ram_principal_association
 output "this_ram_principal_association_id" {
   description = "The Amazon Resource Name (ARN) of the Resource Share and the principal, separated by a comma"
-  value       = element(concat(aws_ram_principal_association.this.*.id, [""]), 0)
+  value       = [ for k, v in aws_ram_principal_association.this : v.id ]
+
 }
