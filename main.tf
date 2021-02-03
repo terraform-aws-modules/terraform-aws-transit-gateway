67b64de00e2632964bfcea0cc32832d5920ede42
Juan Martin Corallo <juanmartin.corallo@sygnum.com>
2021-02-03 16:02:47 +0100

    Optionally allow updating VPC Route Tables for attached VPCs

--- main.tf
+++ main.tf
@@ -19,6 +19,14 @@ locals {
     var.tags,
     var.tgw_default_route_table_tags,
   )
+  vpc_route_table_destination_cidr = flatten([
+    for k, v in var.vpc_attachments : [
+      for rtb_id in lookup(v, "vpc_route_table_ids", []) : {
+        rtb_id = rtb_id
+        cidr   = v["tgw_destination_cidr"]
+      }
+    ]
+  ])
 }
 
 resource "aws_ec2_transit_gateway" "this" {
@@ -74,6 +82,14 @@ resource "aws_ec2_transit_gateway_route" "this" {
 
   transit_gateway_route_table_id = var.create_tgw ? aws_ec2_transit_gateway_route_table.this[0].id : var.transit_gateway_route_table_id
   transit_gateway_attachment_id  = tobool(lookup(local.vpc_attachments_with_routes[count.index][1], "blackhole", false)) == false ? aws_ec2_transit_gateway_vpc_attachment.this[local.vpc_attachments_with_routes[count.index][0]["key"]].id : null
+}
+
+resource "aws_route" "this" {
+  for_each = { for x in local.vpc_route_table_destination_cidr : x.rtb_id => x.cidr }
+
+  route_table_id         = each.key
+  destination_cidr_block = each.value
+  transit_gateway_id     = aws_ec2_transit_gateway.this[0].id
 }
 
 ###########################################################
